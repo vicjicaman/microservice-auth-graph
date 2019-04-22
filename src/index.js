e3eccc949907ba84a7e87a82a998169c4e886151
Victor Jimenez <vic.jicama@gmail.com>
2019-04-22 10:12:02 +0200

    Add microservices build for namespace and service

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+
+require('dotenv').config()
 var express = require('express');
 var express_graphql = require('express-graphql');
 var {
@@ -8,14 +10,17 @@ var {
   resolvers: rootResolvers
 } = require('./schema');
 
+
+console.log(process.env)
+
 const schema = makeExecutableSchema({
   typeDefs: rootSchema,
   resolvers: rootResolvers
 });
 
 var app = express();
-app.use('/', express_graphql({
+app.use(process.env['SERVICE_URL_PATH_GRAPHQL'], express_graphql({
   schema: schema,
   graphiql: true
 }));
-app.listen(4100, () => console.log('Auth GraphQL running...'));
+app.listen(process.env['SERVICE_PORT'], () => console.log('Auth GraphQL running...'));
--- steps.md
+++ steps.md
@@ -1,3 +1,4 @@
 yarn init
 yarn add graphql express express-graphql
 yarn add graphql-tools graphql-tools-types
+yarn add dotenv
