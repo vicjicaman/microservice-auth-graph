edbc1366e9395e8d7dbc1e6db792869248e72956
Victor Jimenez <vic.jicama@gmail.com>
2019-10-28 21:30:18 +0100

    Add logger, update Dockerfile and node version

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const { schema: rootSchema, resolvers: rootResolvers } = require("./schema");
 
 import * as AuthLib from "@nebulario/microservice-auth-common";
 import * as Utils from "@nebulario/microservice-utils";
+import * as Logger from "@nebulario/microservice-logger";
 
 const ACCOUNT_INTERNAL_URL_GRAPH = process.env["ACCOUNT_INTERNAL_URL_GRAPH"];
 const AUTH_CACHE_INTERNAL_HOST = process.env["AUTH_CACHE_INTERNAL_HOST"];
@@ -14,43 +15,49 @@ const AUTH_ROUTE_GRAPH = process.env["AUTH_ROUTE_GRAPH"];
 const AUTH_INTERNAL_PORT_GRAPH = process.env["AUTH_INTERNAL_PORT_GRAPH"];
 const AUTH_CACHE_SECRET_PASSWORD = process.env["AUTH_CACHE_SECRET_PASSWORD"];
 
-var app = express();
+const logger = Logger.create({ path: "/var/log/app" });
+const cxt = { logger };
 
-var { passport } = AuthLib.init({
-  app,
-  cache: {
-    host: AUTH_CACHE_INTERNAL_HOST,
-    port: AUTH_CACHE_INTERNAL_PORT,
-    secret: AUTH_CACHE_SECRET_PASSWORD
-  },
-  accounts: {
-    url: ACCOUNT_INTERNAL_URL_GRAPH
-  }
-});
+(async () => {
+  var app = express();
+  Logger.Service.use(app, cxt);
 
-const schema = makeExecutableSchema({
-  typeDefs: rootSchema,
-  resolvers: rootResolvers
-});
+  var { passport } = AuthLib.init({
+    app,
+    cache: {
+      host: AUTH_CACHE_INTERNAL_HOST,
+      port: AUTH_CACHE_INTERNAL_PORT,
+      secret: AUTH_CACHE_SECRET_PASSWORD
+    },
+    accounts: {
+      url: ACCOUNT_INTERNAL_URL_GRAPH
+    }
+  });
 
-app.use(
-  AUTH_ROUTE_GRAPH,
-  graphqlHTTP(request => ({
-    schema: schema,
-    graphiql: true,
-    context: {
-      passport,
-      request,
-      services: {
-        accounts: {
-          url: ACCOUNT_INTERNAL_URL_GRAPH
+  const schema = makeExecutableSchema({
+    typeDefs: rootSchema,
+    resolvers: rootResolvers
+  });
+
+  app.use(
+    AUTH_ROUTE_GRAPH,
+    graphqlHTTP(request => ({
+      schema: schema,
+      graphiql: true,
+      context: {
+        passport,
+        request,
+        services: {
+          accounts: {
+            url: ACCOUNT_INTERNAL_URL_GRAPH
+          }
         }
       }
-    }
-  }))
-);
-app.listen(AUTH_INTERNAL_PORT_GRAPH, () =>
-  console.log("Auth GraphQL running...")
-);
+    }))
+  );
+  app.listen(AUTH_INTERNAL_PORT_GRAPH, () =>
+    console.log("Auth GraphQL running...")
+  );
+})().catch(e => cxt.logger.error("service.error", { error: e.toString() }));
 
 Utils.Process.shutdown(signal => console.log("shutdown " + signal));
